8dfdad715bad2fdd54af3b2667a96e1a6c501d91
NurcahyoAswinD <aswin.reint@gmail.com>
2023-08-02 14:17:35 +0700

    Update Projek.php

--- app/Models/Projek.php
+++ app/Models/Projek.php
@@ -5,9 +5,8 @@ use Illuminate\Database\Eloquent\Model;
 class Projek extends Model{
     protected $table = "projek";
 
-    function getProjek($select) {
-        return Projek::select($select)
-        ->get();
+    function getProjekAll() {
+        return Projek::get();
     }
 
 
