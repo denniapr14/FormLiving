e22ffe0cc4ea62fe26a911c629e891be4840ebc7
NurcahyoAswinD <aswin.reint@gmail.com>
2023-08-18 13:49:55 +0700

    testing kernel local + printPDF function

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,8 @@ Route::get('/my-cart', [Home::class, 'MyCart']);
 Route::get('/login', [C_Login::class, 'login']);
 Route::post('/login', [C_Login::class, 'loginAction'])->name('login.action');
 Route::get('/logout', [C_Login::class, 'logout']);
+Route::get('/reset-password', [C_Login::class, 'emailForgot']);
+Route::get('/profile/cetak/{code}/{id_formulir}', [Home::class, 'printFP']);
 Route::get('/forgot/{email}', [C_Login::class, 'forgotPassword'])->name('forgot.utama');
 Route::post('/forgot', [C_Login::class, 'forgotAction'])->name('forgot.action');
 
