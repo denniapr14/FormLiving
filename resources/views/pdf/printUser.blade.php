a973132316f5e212fa13c3fbc087901413fa40d5
Nurcahyo Aswin D <aswin.reint@gmail.com>
2023-09-11 16:24:09 +0700

    SPR editing

--- resources/views/pdf/printUser.blade.php
+++ resources/views/pdf/printUser.blade.php
@@ -1,30 +1,27 @@
 <!DOCTYPE html>
 <html>
-
 <head>
 	<title>Laporan User Formsliving via Admin Formsliving</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-
 <body>
 	<style type="text/css">
 		table tr td,
-		table tr th {
+		table tr th{
 			font-size: 9pt;
 		}
 	</style>
 	<center>
 		<h5>laporan User Forms Living Per Tanggal {{ $waktuNow }}</h4>
 	</center>
-
+ 
 	<table class='table table-bordered'>
 		<thead>
 			<tr>
 				<th>No</th>
 				<th>Kode User</th>
 				<th>Nama</th>
-				<th>Email</th>
+				<th>Email</th>	
 				<th> Nomor Telepon</th>
 				<th>Tanggal Daftar</th>
 			</tr>
@@ -43,7 +40,6 @@
 			@endforeach
 		</tbody>
 	</table>
-
+ 
 </body>
-
 </html>
